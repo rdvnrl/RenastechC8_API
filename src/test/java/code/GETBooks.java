f894d9684d6c6a27d54d71f08dbe4e2532659f13
oralridvan <oral.ridvan@hotmail.com>
2023-05-19 21:54:36 -0400

    Api class day 1 part 3

--- src/test/java/code/GETBooks.java
+++ src/test/java/code/GETBooks.java
@@ -2,8 +2,11 @@ package code;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
 
 public class GETBooks {
 
@@ -21,8 +24,67 @@ public class GETBooks {
 
         Assert.assertEquals(actualStatusCode,expectedStatusCode);
     }
+    @Test(description = "Given baseURI When We make a GET call to /books and use query param limit=2 Then verify status code is 200")
+    public void userRetrieveListofTheBooksLimit2(){
 
+        //Given
+        //We will build the request so we that when we make the call all relevant information is ready
+        RequestSpecification request= given().queryParam("limit",2);
 
+        //When - Here we will make the API call
+        Response response = request.when().get(baseURI);
+
+        //Then - Verify
+        response.then().assertThat().statusCode(200);
+
+        //This will print response body
+        System.out.println(response.getBody().asString());
+        //[{"id":1,"name":"The Russian","type":"fiction","available":true},{"id":2,"name":"Just as I Am","type":"non-fiction","available":false}]
+
+        //Print all header?
+        System.out.println(response.getHeaders());
+
+        String actualSecondBookName=response.jsonPath().getString("[1].name");
+        String actualSecondBookId=response.jsonPath().getString("[1].id");
+        int intactualSecondBookId=response.jsonPath().getInt("[1].id");
+
+        String expectedSecondBookName="Just as I Am";
+        String expectedSecondBookId="2";
+
+        System.out.println("actualSecondBookID: "+actualSecondBookId+
+                "ActualSecondBookId int :"+intactualSecondBookId+
+                "Actual Second Book Name"+actualSecondBookName);
+
+        Assert.assertEquals(expectedSecondBookId,actualSecondBookId);
+        Assert.assertEquals(expectedSecondBookName,actualSecondBookName);
+    }
+    @Test(description = "Given baseURI When we make a GET call to /books and provide limit=1 and type=fiction as paramters Then Verify Status code ")
+    void userRetrieveListofBooksMultipleParameters(){
+        //Given
+        RequestSpecification request=given()
+                .queryParams("type","fiction","limit",1);
+
+        //When
+        Response response=request.when().get(baseURI);
+
+        //Then
+        response.then().assertThat().statusCode(200);
+        String type=response.jsonPath().getString("[0].type");
+        System.out.println(type);
+        Assert.assertEquals(type,"fiction");
+
+        String book=response.jsonPath().getString("[0].id");
+        System.out.println(book);
+        Assert.assertEquals(book,"1");
+
+        String isAvailable=response.jsonPath().getString("[0].available");
+        System.out.println(isAvailable);
+        Assert.assertEquals(isAvailable,"true");
+
+      String index0=  response.jsonPath().getString("[0]");
+        System.out.println(index0);
+
+    }
 
 
 
